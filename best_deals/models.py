2597731829cfcfea7254e2be486a6c37fedf1dfd
goldangera <nkirotegolda6888@gmail.com>
2019-10-08 16:39:05 +0300

    maded changes to the models and added a field for categories

--- best_deals/models.py
+++ best_deals/models.py
@@ -6,7 +6,6 @@ class Profile(models.Model):
     avatar = models.ImageField(upload_to='avatars/')
     description = HTMLField()
     username = models.ForeignKey(User,on_delete=models.CASCADE)
-    name =models.CharField(max_length=100)
     email = models.EmailField()
 
     def __str__(self):
@@ -17,6 +16,7 @@ class Business(models.Model):
     logo = models.ImageField(upload_to='businesslogo/')
     description = HTMLField()
     owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    categories = models.CharField(max_length=100,default=None)
     name =models.CharField(max_length=100)
     email = models.EmailField()
     address =models.CharField(max_length=100)
@@ -25,6 +25,6 @@ class Business(models.Model):
     def __str__(self):
         return self.name
     @classmethod
-    def search_businesses(cls,search_term):
-        businesses = cls.objects.filter(Q(name__name=search_term))
+    def search_by_category(cls,search_term):
+        businesses = cls.objects.filter(categories__icontains=search_term)
         return businesses
